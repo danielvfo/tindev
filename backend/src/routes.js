7af744fe3d939adef4e00865a55fad87ecdaf195
danielvfo <danielvfo@gmail.com>
2019-08-06 23:16:24 -0300

    DevController setup to post users to mongo Atlas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,8 @@
 const express = require('express');
+const DevController = require('./controllers/DevController');
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: `Hello ${req.query.name}`});
-});
-
-routes.post('/', (req, res) => {
-  return res.json({ ok: true});
-});
+routes.post('/devs', DevController.store);
 
 module.exports = routes;
