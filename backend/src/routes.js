4e46f186e724ab714767b5b58701835512b98131
danielvfo <danielvfo@gmail.com>
2019-08-06 23:54:37 -0300

    Dislike route and controller setup

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const DevController = require('./controllers/DevController');
 const LikeController = require('./controllers/LikeController');
+const LikeController = require('./controllers/DislikeController');
 
 const routes = express.Router();
 
 routes.post('/devs', DevController.store);
 routes.post('/devs/:devId/likes', LikeController.store);
+routes.post('/devs/:devId/dislikes', LikeController.store);
 
 module.exports = routes;
