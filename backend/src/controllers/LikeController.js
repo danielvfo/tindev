e064356b757f176dd295978b0b6ab99b99bd9c5e
danielvfo <danielvfo@gmail.com>
2019-08-06 23:49:26 -0300

    Configures routes and LikesController

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,9 +2,20 @@ const Dev = require('../models/Dev');
 
 module.exports = {
   async store(req, res) {
-    const { username } = req.body;
+    const { user } = req.headers;
+    const { devId } = req.params;
 
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
 
-    return res.json(dev);
+    if (!targetDev) {
+      return res.status(400).json({ error: 'Dev does not exists' });
+    }
+
+    loggedDev.likes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
   }
 };
