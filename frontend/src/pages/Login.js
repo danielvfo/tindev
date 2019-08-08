fbb83b23b93234b78467d8aa304705f022ac64d0
danielvfo <danielvfo@gmail.com>
2019-08-07 23:25:50 -0300

    Added state and properties to Login component

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/tindev.svg';
 import './Login.css';
 
-export default function Login() {
+export default function Login({ history }) {
+  const [username, setUsername] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(username);
+    history.push('/main');
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <img src={logo} alt="Tindev" />
         <input
           placeholder="Digite seu usuário no GitHub"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
         />
         <button type="submit">Enviar</button>
       </form>
