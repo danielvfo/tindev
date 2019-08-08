e1dc6b03bfee055a6918eaf56a0b71a58425b2e1
danielvfo <danielvfo@gmail.com>
2019-08-07 22:08:13 -0300

    Login page and some styles definition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import logo from './assets/tindev.svg';
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <img src={logo} alt="Tindev" />
+    <Login />
   );
 }
 
