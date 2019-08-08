a0d9555f93e765108435e23014467f2b17ea7f3e
danielvfo <danielvfo@gmail.com>
2019-08-08 00:40:04 -0300

    Main component mockup and Main style

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,8 +1,78 @@
 import React from 'react';
 import logo from '../assets/tindev.svg';
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+import './Main.css';
 
 export default function Main({ match }) {
   return(
-    <h1>{match.params.id}</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
+          <footer>
+            <strong>Daniel</strong>
+            <p>Oloco bicho!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
+          <footer>
+            <strong>Daniel</strong>
+            <p>Oloco bicho!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
+          <footer>
+            <strong>Daniel</strong>
+            <p>Oloco bicho!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+
+        <li>
+          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
+          <footer>
+            <strong>Daniel</strong>
+            <p>Oloco bicho!</p>
+          </footer>
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="Dislike" />
+            </button>
+            <button type="button">
+              <img src={like} alt="Like" />
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 };
