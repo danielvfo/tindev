a451a50088a8a28dab2710fec443f59233a298c7
danielvfo <danielvfo@gmail.com>
2019-08-08 01:25:38 -0300

    Implemented functionality to fetch users from API

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,77 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from '../assets/tindev.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
 import './Main.css';
+import api from '../services/api.js';
 
 export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get('/devs', {
+        headers: {
+          user: match.params.id,
+        }
+      });
+      setUsers(response.data);
+    }
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    console.log('like', id);
+  }
+
+  async function handleDislike(id) {
+    console.log('fislike', id);
+  }
+
   return(
     <div className="main-container">
       <img src={logo} alt="Tindev" />
       <ul>
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
-          <footer>
-            <strong>Daniel</strong>
-            <p>Oloco bicho!</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
-          <footer>
-            <strong>Daniel</strong>
-            <p>Oloco bicho!</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
-          <footer>
-            <strong>Daniel</strong>
-            <p>Oloco bicho!</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
-
-        <li>
-          <img src="https://avatars3.githubusercontent.com/u/5642214?v=4" alt="" />
-          <footer>
-            <strong>Daniel</strong>
-            <p>Oloco bicho!</p>
-          </footer>
-          <div className="buttons">
-            <button type="button">
-              <img src={dislike} alt="Dislike" />
-            </button>
-            <button type="button">
-              <img src={like} alt="Like" />
-            </button>
-          </div>
-        </li>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt="" />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button">
+                <img src={dislike} alt="Dislike" />
+              </button>
+              <button type="button">
+                <img src={like} alt="Like" />
+              </button>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
