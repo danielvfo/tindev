8f16b5a68fce1fb7d3bc7a2a631319bc0148de00
danielvfo <danielvfo@gmail.com>
2019-08-10 20:53:01 -0300

    created and stylized mockup for the Main page

--- app/src/pages/Main.js
+++ app/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  SafeAreaView, StyleSheet
+  View, SafeAreaView, StyleSheet, Text, Image, TouchableOpacity,
 } from 'react-native';
 import logo from '../assets/logo.png';
 import like from '../assets/like.png';
@@ -8,10 +8,116 @@ import dislike from '../assets/dislike.png';
 
 const Main = () => {
   return (
-    <SafeAreaView />
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo}></Image>
+
+      <View style={styles.cardsContainer}>
+        <View style={[styles.card, { zIndex: 3 }]}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/5642214?v=4' }}></Image>
+          <View style={styles.footer}>
+            <Text style={styles.name}>Daniel</Text>
+            <Text numberOfLines={3} style={styles.bio}>Oloco Bicho!</Text>
+          </View>
+        </View>
+
+        <View style={[styles.card, { zIndex: 2 }]}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/5642214?v=4' }}></Image>
+          <View style={styles.footer}>
+            <Text style={styles.name}>Daniel</Text>
+            <Text numberOfLines={3} style={styles.bio}>Oloco Bicho!</Text>
+          </View>
+        </View>
+
+        <View style={[styles.card, { zIndex: 1 }]}>
+          <Image style={styles.avatar} source={{ uri: 'https://avatars3.githubusercontent.com/u/5642214?v=4' }}></Image>
+          <View style={styles.footer}>
+            <Text style={styles.name}>Daniel</Text>
+            <Text numberOfLines={3} style={styles.bio}>Oloco Bicho!</Text>
+          </View>
+        </View>
+      </View>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Image source={dislike}></Image>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image source={like}></Image>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  cardsContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+    maxHeight: 500,
+  },
+  card: {
+    borderWidth: 1,
+    borderColor: '#dddddd',
+    borderRadius: 8,
+    margin: 30,
+    overflow: 'hidden',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+  },
+  footer: {
+    backgroundColor: '#ffffff',
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333333',
+  },
+  bio: {
+    fontSize: 14,
+    marginTop: 5,
+    lineHeight: 18,
+    color: '#999999',
+  },
+  avatar: {
+    flex: 1,
+    height: 300,
+  },
+  logo: {
+    marginTop: 30,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    marginBottom: 30,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#ffffff',
+    marginHorizontal: 20,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 2,
+    shadowColor: '#000000',
+    shadowOpacity: 0.05,
+    shadowRadius: 2,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    }
+  },
+});
 
 export default Main;
