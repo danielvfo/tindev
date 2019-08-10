541087d3b728408bfc41d6f00376c7315b1219e2
danielvfo <danielvfo@gmail.com>
2019-08-10 18:46:47 -0300

    added dependencies animated gesture navigation

---
+++ app/src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import logo from '../assets/logo.png';
+
+const Main = () => {
+  return (
+    <View />
+  );
+};
+
+export default Main;
