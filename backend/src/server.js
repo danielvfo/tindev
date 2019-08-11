142200be34e4d0bfeb04f3b423375d724d5ae76c
danielvfo <danielvfo@gmail.com>
2019-08-11 16:19:47 -0300

    socket.io backend setupt

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,20 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const cors = require('cors');
 
-const server = express();
+const httpServer = express();
+const server = require('http').Server(httpServer);
+const io = require('socket.io');
+
+io.on('connection', socket => {
+  console.log('Nova conexão', socket.id);
+});
 
 mongoose.connect('mongodb+srv://tindev:tindevloko@dancluster-logmj.gcp.mongodb.net/tindev?retryWrites=true&w=majority', {
   useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+httpServer.use(cors());
+httpServer.use(express.json());
+httpServer.use(routes);
 
 server.listen(3333);
